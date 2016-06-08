5bf3ef11c325e4763f6762f097e30d69297463a6
Akash Trivedi <akashtrivedi@Akashs-MacBook-Air.local>
2016-06-08 01:01:42 -0700

    updated mongodb uri

--- server/config/server.js
+++ server/config/server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 //connect to db
-mongoose.connect('mongodb://ds037095.mlab.com:37095/reacttodoapp');
+mongoose.connect('mongodb://kash1215:Bboo8276@ds037095.mlab.com:37095/reacttodoapp');
 
 
 //create instance of express server
